c0593e91f070b9d07e47cb237b8951a7b7601b6a
Creariax5 <63298524+Creariax5@users.noreply.github.com>
2023-01-02 17:19:13 +0100

    add name pk api and username

--- client/views.py
+++ client/views.py
@@ -1,3 +1,5 @@
+import requests
+import json
 from django.shortcuts import render, redirect
 from _datetime import datetime
 from .formulaire import Students_from, Students
@@ -11,10 +13,29 @@ def indexClient(request):
         form = Students_from
     modeFormat = "client"
     date = datetime.today()
+
+    url = "https://pokemon-go1.p.rapidapi.com/pokemon_names.json"
+
+    headers = {
+        "X-RapidAPI-Key": "2791ff7022mshc7cf931913ff7d6p16c6eejsna69458bab2e5",
+        "X-RapidAPI-Host": "pokemon-go1.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    pks = response.text
+    aDict = json.loads(pks)
+    my_list = []
+    i = 1
+    while i != 906:
+        my_list.append(aDict[str(i)]['name'])
+        i += 1
+
     return render(request, "indexClient.html", context={"date": date,
                                                         "modeFormat": modeFormat,
                                                         "form": form,
-                                                        "dataStudents": Students.objects.all()})
+                                                        "dataStudents": Students.objects.all(),
+                                                        "response": my_list})
 
 
 def demande(request):
--- relaigraph/views.py
+++ relaigraph/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    current_user = request.user
+    admin = False
+    if current_user.username == 'admin':
+        admin = True
+    my_id = current_user.id
+    return render(request, "index.html", context={"admin": admin,
+                                                  "id": my_id})
