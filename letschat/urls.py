4ef476d0db674d319169d38775634f03721492da
Creariax5 <63298524+Creariax5@users.noreply.github.com>
2023-01-28 21:11:43 +0100

    chat style apply

--- letschat/urls.py
+++ letschat/urls.py
@@ -5,6 +5,4 @@ app_name = 'letschat_app'
 urlpatterns = [
     path('chatroom/', letschat),
     path('<str:room>/', room),
-    path('send', send, name='send'),
-    path('getMessages/<str:room>/', getMessages, name='getMessages'),
 ]
--- letschat/views.py
+++ letschat/views.py
@@ -1,8 +1,4 @@
-import time
-
 from django.shortcuts import render, redirect
-from .models import Message
-from django.http import HttpResponse, JsonResponse
 
 
 def letschat(request):
@@ -17,25 +13,3 @@ def room(request, room):
         'room': room,
         'room_details': room_details
     })
-
-
-def send(request):
-    message = request.POST['message']
-    username = request.POST['username']
-    room_id = request.POST['room_id']
-
-    new_message = Message.objects.create(value=message, user=username, room=room_id)
-    new_message.save()
-    return HttpResponse('Message sent successfully')
-
-
-def getMessages(request, room):
-    last_messages = Message.objects.all().filter(room=room, last=0)
-    my_list = list(last_messages.values())
-    print(my_list)
-    if last_messages:
-        for m in last_messages:
-            m.last = 1
-            m.save()
-
-    return JsonResponse({"messages": my_list})
