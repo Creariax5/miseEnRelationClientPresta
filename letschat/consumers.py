1017631f7eb573187075825ae8a0c1d3c695f654
Creariax5 <63298524+Creariax5@users.noreply.github.com>
2023-01-29 19:18:56 +0100

    chat bug correction

--- letschat/consumers.py
+++ letschat/consumers.py
@@ -17,19 +17,23 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        user = text_data_json['user']
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': user
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        user = event['user']
 
         self.send(text_data=json.dumps({
             'type': 'chat',
-            'message': message
+            'message': message,
+            'user': user
         }))
